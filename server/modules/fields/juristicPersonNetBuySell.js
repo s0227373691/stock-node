0f04b90a41bbeb79c068df7363f2b6d1343b759a
Elmer <s0227373691@gms.tku.edu.tw>
2020-09-16 02:11:38 +0800

    remove comment

--- server/modules/fields/juristicPersonNetBuySell.js
+++ server/modules/fields/juristicPersonNetBuySell.js
@@ -3,19 +3,15 @@ const mongoose = require('mongoose');
 const juristicPersonNetBuySell = new mongoose.Schema({
     stock_code: {
         type: String,
-        // required,
     },
     stock_name: {
         type: String,
-        // required,
     },
     buy_shares: {
         type: String,
-        // required,
     },
     sell_shares: {
         type: String,
-        // required,
     },
     date: {
         type: Date,
