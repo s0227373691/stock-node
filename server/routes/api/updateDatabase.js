96c77f7412098503de2eeb72a2ee259abf277496
Elmer <s0227373691@gms.tku.edu.tw>
2020-09-21 16:31:53 +0800

    更改下載資料條件

--- server/routes/api/updateDatabase.js
+++ server/routes/api/updateDatabase.js
@@ -5,31 +5,41 @@ const router = express.Router();
 
 const JuristicPerson = require('../../modules/juristicPersonNetBuySell');
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
     const now = new Date();
-    const today = dateformat(now, 'yyyymmdd');
-    const today = '20200916';
+    fetchData();
+    async function fetchData() {
+        //判斷DB是否有當天資料
+        now.setDate(now.getDate() - 1);
 
-    const juristicPerson = await JuristicPerson.findOne({ date: today });
-
-    //判斷DB是否有當天資料
-    if (juristicPerson) { //TODO 傳入當天日期檢查今天以前資料庫是否有資料
-        console.log('y');
-    } else {
-        console.log('n');
-        const URL = `https://www.twse.com.tw/fund/TWT44U?response=json&date=${today}`;
-        const {
-            data: { date, fields, data },
-        } = await axios.get(URL);
-        const juristicPersonModule = new JuristicPerson({
-            date,
-            fields,
-            data,
-        });
-        await juristicPersonModule.save();
+        //判斷是否為周末
+        const day = now.getDay().toString();
+        if (day === '0' || day === '6') {
+            fetchData();
+        } else {
+            const today = dateformat(now, 'yyyymmdd');
+            console.log(today, day);
+            const juristicPerson = await JuristicPerson.findOne({
+                date: today,
+            });
+            if (!juristicPerson) {
+                //TODO 傳入當天日期檢查今天以前資料庫是否有資料
+                const URL = `https://www.twse.com.tw/fund/TWT44U?response=json&date=${today}`;
+                const {
+                    data: { date, fields, data },
+                } = await axios.get(URL);
+                const juristicPersonModule = new JuristicPerson({
+                    date,
+                    fields,
+                    data,
+                });
+                await juristicPersonModule.save();
+                await setTimeout(() => fetchData(), 800);
+            }
+        }
     }
 
-    res.send('');
+    res.send('更新完成!!');
 });
 
 module.exports = router;
